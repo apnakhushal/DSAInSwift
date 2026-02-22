95d6b528c70eb4851d8add3c832676fb31aeefcf
Khushal Jhalani <khushaljhalani@gmail.com>
2026-02-22 19:36:37 +0400

    2 Pointers - Problem No. 2: Container with most water

---
+++ 2Pointers/ContainerWithMostWater.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+func containerWithMostWater(_ heights: [Int]) -> Int {
+    var maxArea = 0
+    var left = 0
+    var right = heights.count - 1
+    while left <= right {
+        let currentArea = min(heights[left], heights[right]) * (right - left)
+        if maxArea < currentArea {
+            maxArea = currentArea
+        }
+        
+        if heights[left] < heights[right] {
+            left += 1
+        } else {
+            right -= 1
+        }
+    }
+    
+    return maxArea
+}
+
+print(containerWithMostWater([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+print(containerWithMostWater([1, 4, 9, 2, 5, 4, 8, 10, 7]))
